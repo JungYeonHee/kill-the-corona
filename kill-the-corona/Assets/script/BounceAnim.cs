2570fae60e8bab066ee1e5d2c56da8d8a6736485
JungYeonHee <dusgml5328@gmail.com>
2021-02-12 16:44:09 +0900

    kill-the-corona
    kill-the-corona

---
+++ kill-the-corona/Assets/script/BounceAnim.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BounceAnim : MonoBehaviour
+{
+    float time = 0;
+    public float _size = 1;
+
+    public float _upSizeTime =0.3f;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        Amin();
+    }
+
+    public void Amin()
+    {
+        if (time <= _upSizeTime)
+        {
+            transform.localScale = Vector3.one * (1 + _size * time);
+        }
+        else if (time <= _upSizeTime * 2)
+        {
+            transform.localScale = Vector3.one * (2 * _size * _upSizeTime + 1 - time * _size);
+        }
+        else
+        {
+            transform.localScale = Vector3.one;
+            time = 0;
+        }
+        time += Time.deltaTime;
+
+    }
+
+
+    
+
+}
